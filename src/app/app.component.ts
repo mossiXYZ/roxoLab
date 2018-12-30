aa11d17ab8877234e9163169a04ef7b91f6a1ffc
mossi <mossi.hashemi@gmail.com>
2018-12-30 22:14:52 +0100

    part2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'roxoLab';
+
+  newServerName ='';
+  newServerContent ='';
+
+
+  onAddPersianServer(){
+
+  }
+  
+  onAddFrenchServer(){
+    
+  }
+
 }
